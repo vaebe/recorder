831b8e0db0019f5a7c9e2d8a4a2f0d761e9a2491
vaebe <18137693952@163.com>
2024-05-30 23:05:10 +0800

    feat: 修改发布 npm 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import copyPackageJsonPlugin from './plugin/copyPackageJson'
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true }), copyPackageJsonPlugin()],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
